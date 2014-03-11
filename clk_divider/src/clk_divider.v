c6f77f8f1ce0b4c66a5982a34ab6003ae40d821a
ka04 <karen.anundson@gmail.com>
2014-03-11 19:59:48 -0400

    Updated clock divider to have a wider counter.

--- clk_divider/src/clk_divider.v
+++ clk_divider/src/clk_divider.v
@@ -25,14 +25,14 @@ module clk_divider(
     
    parameter P_DIV_FACTOR = 4;
 
-   reg [19:0] clk_counter = 1;
+   reg [27:0] clk_counter = 1;
    reg        tmp_clk = 0;
 
 	// Convert 100 MHz input clock to 25MHz clock for display 
 	always @(posedge i_clk)
 	begin
       if (clk_counter < (P_DIV_FACTOR/2)) begin
-			clk_counter <= clk_counter+{18'b0, 1'b1};
+			clk_counter <= clk_counter+{26'b0, 1'b1};
 		end else begin 
 			clk_counter <= 1;
 			tmp_clk     <= ~tmp_clk;
